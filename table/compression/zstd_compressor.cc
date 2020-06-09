125d324b7e6cf7ccb044afadd16bb8c58d6f4cd5
Dylan K. Taylor <odigiman@gmail.com>
2020-06-09 12:19:33 +0100

    zstd: double performance of decompression

--- table/compression/zstd_compressor.cc
+++ table/compression/zstd_compressor.cc
@@ -24,27 +24,17 @@ void leveldb::ZstdCompressor::compressImpl(const char* input, size_t length,
 
 bool leveldb::ZstdCompressor::decompress(const char* input, size_t length,
                                          ::std::string& output) const {
-  // extend the buffer to contain the worst case. Worst case is that input
-  // length == output length
-  auto originalSize = output.size();
-
-  auto bufsize = length;
-
-  while (true) {
-    bufsize *= 10;  // assume that the compression is compressing worse than
-                    // 10%.
-    // TODO use streams to decompress piece by piece, this is pretty wasteful
-    // for memory & re-decompressions
-    output.resize(originalSize + bufsize);
-
-    auto sz = ZSTD_decompress((void*)(output.data() + originalSize), bufsize,
-                              input, length);
-    if (!ZSTD_isError(sz)) {
-      output.resize(sz + originalSize);
-      break;
-    }
+  auto decompressedSize = ZSTD_getFrameContentSize(input, length);
+  if (ZSTD_isError(decompressedSize)) {
+    return false;
   }
+  output.resize(decompressedSize);
 
+  auto sz = ZSTD_decompress((void*)output.data(), output.size(), input, length);
+  if (ZSTD_isError(sz)) {
+    return false;
+  }
+  output.resize(sz);
   return true;
 }
 
